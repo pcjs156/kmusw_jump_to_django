93a4ca344501802ade7aa7641a3f3b8e67397193
Yubin Park <itqkrdbqls1001@naver.com>
2021-08-05 23:44:30 +0900

    Add Answer create

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from pybo.models import Question
+from pybo.models import Question, Answer
 
 
 class QuestionForm(forms.ModelForm):
@@ -10,4 +10,13 @@ class QuestionForm(forms.ModelForm):
         labels = {
             'subject': '제목',
             'content': '내용',
+        }
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변 내용',
         }
