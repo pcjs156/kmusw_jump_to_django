29052c68cbf5c12bbd50157c0986896229e6a775
Yubin Park <itqkrdbqls1001@naver.com>
2021-08-05 20:38:25 +0900

    override __str__ method of Question, Answer Model

--- pybo/models.py
+++ pybo/models.py
@@ -6,8 +6,16 @@ class Question(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
 
+    def __str__(self):
+        subject = self.subject if len(self.subject) < 10 else self.subject[:10] + '...'
+        return f"{self.pk}) {subject} on {self.create_date}"
+
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+
+    def __str__(self):
+        content = self.content if len(self.content) < 10 else self.content[:10] + '...'
+        return f"{self.pk}) {content} on {self.create_date} / RE: {self.question}"
