7a117d3ef7fe35cb8c94c2fcf0dd0fa7493cbf98
Yubin Park <itqkrdbqls1001@naver.com>
2021-08-28 14:53:07 +0900

    feat(pybo): 질문/답변 검색 기능 구현

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,4 +1,5 @@
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
 from pybo.models import Question
@@ -10,15 +11,24 @@ def index(request):
     """
     # 페이지
     page = request.GET.get('page', '1')
+    # 검색어
+    kw = request.GET.get('kw', '')
 
     # 조회
     question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목검색
+            Q(content__icontains=kw) |  # 내용검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이검색
+        ).distinct()
 
     # 페이징 (페이지 당 10건)
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
     return render(request, 'pybo/question_list.html', context)
 
 
