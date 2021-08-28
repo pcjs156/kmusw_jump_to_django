f7035e5acd3477190000ce16b1434a67bfa2f8e7
Yubin Park <itqkrdbqls1001@naver.com>
2021-08-28 14:58:41 +0900

    feat(pybo): 질문 정렬 기능 구현

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.db.models import Q
+from django.db.models import Q, Count
 from django.shortcuts import render, get_object_or_404
 
 from pybo.models import Question
@@ -13,9 +13,17 @@ def index(request):
     page = request.GET.get('page', '1')
     # 검색어
     kw = request.GET.get('kw', '')
+    # 정렬 기준
+    so = request.GET.get('so', 'recent')
 
-    # 조회
-    question_list = Question.objects.order_by('-create_date')
+    # 조회 및 정렬
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:  # recent
+        question_list = Question.objects.order_by('-create_date')
+
     if kw:
         question_list = question_list.filter(
             Q(subject__icontains=kw) |  # 제목검색
