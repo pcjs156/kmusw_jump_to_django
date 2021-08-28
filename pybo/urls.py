79c56453c99880dd493841c5b56435f53b8777f0
Yubin Park <itqkrdbqls1001@naver.com>
2021-08-28 14:19:41 +0900

    feat(pybo): 답변 추천 기능 구현

--- pybo/urls.py
+++ pybo/urls.py
@@ -40,7 +40,8 @@ comment_urlpatterns = [
 
 # vote_views.py
 vote_urlpatterns = [
-    path('vote/question/<int:question_id>/', vote_views.vote_question, name='vote_question'),
+    path('question/<int:question_id>/', vote_views.vote_question, name='vote_question'),
+    path('answer/<int:answer_id>/', vote_views.vote_answer, name='vote_answer'),
 ]
 
 urlpatterns = [
