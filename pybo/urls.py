3cd5b95a0b89a1c8e0adf39c2b629ac7ae80ea6c
Yubin Park <itqkrdbqls1001@naver.com>
2021-08-28 14:15:37 +0900

    refactor(pybo): urls.py 구조 변경 (path include 방식으로 개선)

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,36 +1,52 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import base_views, question_views, answer_views, comment_views, vote_views
 
 app_name = 'pybo'
 
-urlpatterns = [
-    # base_views.py
+# base_views.py
+base_urlpatterns = [
     path('', base_views.index, name='index'),
     path('<int:question_id>/', base_views.detail, name='detail'),
+]
 
-    # question_views.py
-    path('question/create/', question_views.question_create, name='question_create'),
-    path('question/modify/<int:question_id>/', question_views.question_modify, name='question_modify'),
-    path('question/delete/<int:question_id>/', question_views.question_delete, name='question_delete'),
+# question_views.py
+question_urlpatterns = [
+    path('create/', question_views.question_create, name='question_create'),
+    path('modify/<int:question_id>/', question_views.question_modify, name='question_modify'),
+    path('delete/<int:question_id>/', question_views.question_delete, name='question_delete'),
+]
 
-    # answer_views.py
-    path('answer/create/', answer_views.answer_create, name='answer_create'),
-    path('answer/create/<int:question_id>/', answer_views.answer_create, name='answer_create'),
-    path('answer/modify/<int:answer_id>/', answer_views.answer_modify, name='answer_modify'),
-    path('answer/delete/<int:answer_id>/', answer_views.answer_delete, name='answer_delete'),
+# answer_views.py
+answer_urlpatterns = [
+    path('create/', answer_views.answer_create, name='answer_create'),
+    path('create/<int:question_id>/', answer_views.answer_create, name='answer_create'),
+    path('modify/<int:answer_id>/', answer_views.answer_modify, name='answer_modify'),
+    path('delete/<int:answer_id>/', answer_views.answer_delete, name='answer_delete'),
+]
 
-    # comment_views.py
-    path('comment/create/question/<int:question_id>/', comment_views.comment_create_question,
+# comment_views.py
+comment_urlpatterns = [
+    path('create/question/<int:question_id>/', comment_views.comment_create_question,
          name='comment_create_question'),
-    path('comment/modify/question/<int:comment_id>/', comment_views.comment_modify_question,
+    path('modify/question/<int:comment_id>/', comment_views.comment_modify_question,
          name='comment_modify_question'),
-    path('comment/delete/question/<int:comment_id>/', comment_views.comment_delete_question,
+    path('delete/question/<int:comment_id>/', comment_views.comment_delete_question,
          name='comment_delete_question'),
-    path('comment/create/answer/<int:answer_id>/', comment_views.comment_create_answer, name='comment_create_answer'),
-    path('comment/modify/answer/<int:comment_id>/', comment_views.comment_modify_answer, name='comment_modify_answer'),
-    path('comment/delete/answer/<int:comment_id>/', comment_views.comment_delete_answer, name='comment_delete_answer'),
+    path('create/answer/<int:answer_id>/', comment_views.comment_create_answer, name='comment_create_answer'),
+    path('modify/answer/<int:comment_id>/', comment_views.comment_modify_answer, name='comment_modify_answer'),
+    path('delete/answer/<int:comment_id>/', comment_views.comment_delete_answer, name='comment_delete_answer'),
+]
 
-    # vote_views.py
+# vote_views.py
+vote_urlpatterns = [
     path('vote/question/<int:question_id>/', vote_views.vote_question, name='vote_question'),
+]
+
+urlpatterns = [
+    path('', include(base_urlpatterns)),
+    path('question/', include(question_urlpatterns)),
+    path('answer/', include(answer_urlpatterns)),
+    path('comment/', include(comment_urlpatterns)),
+    path('vote/', include(vote_urlpatterns)),
 ]
